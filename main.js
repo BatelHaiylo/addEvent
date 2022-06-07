7dc2a8e600c25582c4a2985b17cbeb8b7a7fd709
Batel Haiylo <inpo686@gmail.com>
2022-06-07 09:13:59 +0300

    excs 5

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// // קוד דינאמי 
+// // קוד דינאמי
 
 // var numberArray = [54,22,22,44,5,66,2,3,55,9,54,2,132];
 // for(i = 0; i<numberArray.length; i++){
@@ -14,7 +14,6 @@
 //         document.getElementById("div_"+i).style.color = "blue";
 //     }
 // }
-
 
 // // קוד נקי וגינרי
 
@@ -45,124 +44,204 @@
 //         if(valuesArray[i].includes(searchInput)){
 //            results.innerText += valuesArray[i] + '\n';
 //         }
-//     }    
+//     }
+// }
+
+var input1 = document.createElement("input");
+document.body.append(input1);
+input1.id = "userBirtDate";
+input1.type = "date";
+input1.placeholder = "please enter your date";
+
+var breakLine1 = document.createElement("br");
+document.body.append(breakLine1);
+
+var input2 = document.createElement("input");
+document.body.append(input2);
+input2.id = "userName";
+input2.type = "text";
+input2.placeholder = "please enter your name";
+
+var breakLine2 = document.createElement("br");
+document.body.append(breakLine2);
+
+var button = document.createElement("button");
+document.body.append(button);
+button.innerText = "send";
+button.addEventListener("click", program);
+
+var breakLine3 = document.createElement("br");
+document.body.append(breakLine3);
+
+function program() {
+  var todaysDate = new Date();
+
+  var h1 = document.createElement("h1");
+  document.body.append(h1);
+
+  h1.addEventListener("mouseover", function () {
+    h1.style.color = "green";
+  });
+  setTimeout(refresh, 5000);
+
+  if (todaysDate.getFullYear() - getStringDateyear(input1.value) > 18) {
+    h1.innerText = "welcom" + input2.value;
+    h1.style.color = "blue";
+    button.disabled = true;
+  } else {
+    h1.innerHTML =
+      "<div>" +
+      "NO ENTRY" +
+      "</div>" +
+      input2.value +
+      "\n" +
+      "<img src=" +
+      "https://i.gifer.com/7Kyt.gif" +
+      ">";
+    h1.style.color = "red";
+    button.disabled = true;
+  }
+}
+
+function getStringDateyear(str) {
+  var year = "";
+
+  for (i = 0; i < str.length; i++) {
+    year += str[i];
+    if (str[i] == "-") return parseInt(year);
+  }
+}
+
+function refresh() {
+  window.location.reload();
+}
+
+var inputLastName = document.createElement("input");
+document.body.append(inputLastName);
+inputLastName.type = "text";
+inputLastName.placeholder = "please enter your last name";
+
+var firstNames = document.createElement("input");
+document.body.append(firstNames);
+firstNames.id = "firstName";
+firstNames.type = "text";
+firstNames.placeholder = "please enter your 4 first names";
+
+var nameButton = document.createElement("button");
+document.body.append(nameButton);
+nameButton.innerText = "submit";
+
+var listTag = document.createElement("ul");
+document.body.append(listTag);
+// listTag.style.listStyleType = "none"
+
+var firstNamesArray = [];
+
+function getNamesFromUser() {
+  var firstNamesStr = firstNames.value;
+  var text = "";
+
+  for (i = 0; i < firstNamesStr.length; i++) {
+    if (
+      firstNamesStr[i] != " " ||
+      firstNamesStr[i] == firstNamesStr[firstNamesStr.length - 1]
+    ) {
+      text += firstNamesStr[i];
+    } else {
+      firstNamesArray.push(text);
+      text = "";
+    }
+  }
+  firstNamesArray.push(text);
+
+  for (i = 0; i < firstNamesArray.length; i++) {
+    var listItem = document.createElement("li");
+    listItem.innerText += firstNamesArray[i] + " " + inputLastName.value;
+    
+    var str = firstNamesArray[i];
+    if(input2.value.startsWith(str[0])){
+        listItem.style.color = "orange";
+    }
+    listTag.appendChild(listItem);
+
+    }
+    var searchInputBlock = document.createElement("input");
+    document.body.append(searchInputBlock);
+    searchInputBlock.type = "search";
+    searchInputBlock.placeholder = "search a name here";
+
+    var p = document.createElement("p");
+    document.body.append(p);
+
+    var serchValue = searchInputBlock.value;
+
+    for(j=0; j<serchValue.length; i++){
+        if(str.includes(serchValue)){
+            p.innerText += str + '\n';
+         }
+    }
+}
+
+
+    
+
+
+
+nameButton.addEventListener("click", () => {
+  getNamesFromUser();
+});
+
+
+var createInputButton = document.createElement("button");
+document.body.append(createInputButton);
+createInputButton.innerText = "create";
+createInputButton.addEventListener("click", () =>{
+    creatingSearchInput()
+})
+
+
+// var p = document.createElement("p");
+// document.body.append(p);
+
+// function creatingSearchInput() {
+//   var searchInputBlock = document.createElement("input");
+//   document.body.append(searchInputBlock);
+//   searchInputBlock.type = "search";
+//   searchInputBlock.placeholder = "search a name here";
+// //   searchInputBlock.oninput = "searchMiniSimpleAlgoritem()";
+//   searchInputBlock.addEventListener("input", 
+//   function searchMiniSimpleAlgoritem() {
+//     for (var i = 0; i < firstNamesArray.length; i++) {
+//       if (firstNamesArray[i].includes(searchInputBlock.value)) {
+//         p.innerText += array[i] + "\n";
+//       }
+//     }
+//   }
+//   )
+
 // }
 
 
+// function searchFunc () {
 
-var input1 = document.createElement("input")
-document.body.append(input1)
-input1.id = "userBirtDate"
-input1.type = "date"
-input1.placeholder = "please enter your date"
+//     var searchInputBlock = document.createElement("input");
+//     document.body.append(searchInputBlock);
+//     searchInputBlock.type = "search";
+//     searchInputBlock.placeholder = "search a name here";
+//     searchInputBlock.innerText = "";
 
-var breakLine1 = document.createElement("br")
-document.body.append(breakLine1)
+//     for (let i = 0; i < firstNamesArray.length; i++) {
+//         if(firstNamesArray[i].includes(searchInput)){
+//            results.innerText += valuesArray[i] + '\n';
+//         }
+//     }
+// }
 
-var input2 = document.createElement("input")
-document.body.append(input2)
-input2.id = "userName"
-input2.type = "text"
-input2.placeholder = "please enter your name"
+// function searchMiniSimpleAlgoritem() {
+//   for (var i = 0; i < firstNamesArray.length; i++) {
+//     if (firstNamesArray[i].includes(searchInputBlock.value)) {
+//       p.innerText += array[i] + "\n";
+//     }
+//   }
+// }
 
-var breakLine2 = document.createElement("br")
-document.body.append(breakLine2)
-
-var button = document.createElement("button")
-document.body.append(button)
-button.innerText = "send"
-button.addEventListener("click" , program)
-
-var breakLine3 = document.createElement("br")
-document.body.append(breakLine3)
-
-
-function program(){
-    var todaysDate = new Date ()
-   
-    var h1 = document.createElement("h1")
-    document.body.append(h1)
-
-    h1.addEventListener("mouseover" , function(){h1.style.color = "green"})
-    setTimeout(refresh,5000)
-
-    if(todaysDate.getFullYear()- getStringDateyear(input1.value)>18){
-        h1.innerText = "welcom" +input2.value
-        h1.style.color = "blue"
-        button.disabled = true
-    }
-    else{
-        h1.innerHTML ="<div>"+"NO ENTRY"+"</div>"+input2.value +"\n"+
-                      "<img src="+  "https://i.gifer.com/7Kyt.gif" + ">"
-        h1.style.color = "red"
-        button.disabled = true
-    }
-}
-
-function getStringDateyear(str){
-    var year = ""
-
-    for(i=0; i<str.length; i++){
-        year+=str[i]
-        if(str[i] == "-")
-        return parseInt(year)
-    } 
-}
-
-function refresh (){
-    window.location.reload();
-}
-
-
-
-var inputLastName = document.createElement("input")
-document.body.append(inputLastName)
-inputLastName.type = "text"
-inputLastName.placeholder = "please enter your last name"
-
-var firstNames = document.createElement("input")
-document.body.append(firstNames)
-firstNames.id = "firstName"
-firstNames.type = "text"
-firstNames.placeholder = "please enter your 4 first names"
-
-
-var nameButton = document.createElement("button")
-document.body.append(nameButton)
-nameButton.innerText = "submit"
-
-var listTag = document.createElement("ul")
-document.body.append(listTag)
-// listTag.style.listStyleType = "none" 
-
-nameButton.addEventListener("click" , () => {getOrgainInList (firstNamesArray, listTag)}) 
-
-var firstNamesArray = []
-
-function getNamesFromUser () {
-
-    var firstNamesStr = firstNames.value  
-    var text = ""
-    
-    for(i = 0; i<firstNamesStr.length; i++){
-        if(firstNamesStr[i] != " " || firstNamesStr[i] == firstNamesStr[(firstNamesStr.length)-1]){
-            text += firstNamesStr[i]
-        }
-        else{
-            firstNamesArray.push(text)
-            text = ""
-        }
-    }
-    firstNamesArray.push(text)
-}
-function getOrgainInList (arry){
-// function getOrgainInList (arry,element){
-
-    getNamesFromUser ()
-    for(var i = 0; i<arry.length; i++){
-        element.innerHtml += "<li>"+ arry[i] +" "+ inputLastName.value + "</li>";
-        // document.body.innerHtml += "<h1>"+ arry[i] +" "+ inputLastName.value + "</h1>";
-    }
-}
-
-// document.body.innerHtml += "<h1 id= 'h1_"+i+1+"'>"+ arry[i] +" "+ inputLastName.value + "</h1>";
